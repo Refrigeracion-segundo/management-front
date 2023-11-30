3c060d94b5ce4358ebd96b56cfcf80cef6e18f11
Victor Armenta <varmenta.98.vaa@gmail.com>
2023-11-30 10:34:09 -0700

    change api url

--- src/redux/constants/index.ts
+++ src/redux/constants/index.ts
@@ -1,4 +1,3 @@
-// TODO: CHANGE
-export const API_URL = "https://bxbsdrhz-3333.usw3.devtunnels.ms/v1";
-// "https://canberra-headless-chicken-monster-mjnn.1.us-1.fl0.io/v1";
+export const API_URL =
+  "https://canberra-headless-chicken-monster-mjnn.1.us-1.fl0.io/v1";
 export * from "./status";
